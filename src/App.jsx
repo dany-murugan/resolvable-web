f6b695f665927e3680832812fc0fe3dec5b47a03
FirstDot <dm.firstdot@gmail.com>
2023-04-24 11:59:06 +0400

    [API] Activate Swagger Editor API

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,19 @@ function App() {
   const [agentLists, setAgentLists] = useState(null);
   
   const api_call = async() => { 
-    const url = `https://dummyapi.io/data/v1/user?limit=10`;
-    const headers = { 'app-id': '643fc8bbe214cb4bfbbf8c76' };
-    const request = axios.get(url, {headers});
 
-    // const url = `http://134.122.98.10/api/invite/yPJFdmhW3nThQKhh`;
-    // const headers = { 'api_key': '643fc8bbe214cb4bfbbf8c76' };
+    // Dummy API
+    // const url = `https://dummyapi.io/data/v1/user?limit=10`;
+    // const headers = { 'app-id': '643fc8bbe214cb4bfbbf8c76' };
     // const request = axios.get(url, {headers});
 
+    // Swagger Editor
+    const url = `http://134.122.98.10/api/invite/yPJFdmhW3nThQKhh`;
+    const request = axios.get(url);
     const response = await request;
-    console.log('response', response.data)
+    console.log('response', response.data);
 
-
-    setAgentLists(response.data.data);
+    // setAgentLists(response.data.data);
   }
 
   useEffect(() => {
