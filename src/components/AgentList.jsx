73a2fbb4698564809c5929167be3d46afa02ef4c
Dany MURUGAN <dmurugan@zam0q3759.bbdo.org>
2023-04-25 16:00:38 +0400

    Code clean

--- src/components/AgentList.jsx
+++ src/components/AgentList.jsx
@@ -14,126 +14,54 @@ const AgentList = () => {
   const [commentValue, setCommentValue] = useState("");
   const [rnrDone, setrnrDone] = useState(false);
 
-  {agentLists && console.log("agentLists", agentLists);}
-  // {agentLists && console.log("agentLists", agentLists);}
-
   // Submit Rating & Comment
   const handleSubmit = () => {
-    // const newUpdateAgent = {
-    //   firstName: agentLists.first_name,
-    //   lastName: agentLists.last_name,
-    //   picture: agentLists.picture_url,
-    //   rating: rateValue,
-    //   comment: commentValue,
-    // };
 
-    const ratingScore = {
-      "score": rateValue
-    }
-
-    // const api_post = async () => {
-    //   const url = `http://134.122.98.10/api/invite/${agentID}/rate`;
-    //   axios.post(url, ratingScore);
-
-      console.log("agentID", agentID);
-    //   console.log("url", url);
-    // };
-
-    // const API_URL = `http://134.122.98.10/api/invite/FhuDa11DTGiq9cvX/rate`;
-
-    // ******** axios api *****************//
-    // const postData = async (data) => {
-    //   try {
-    //     const config = {
-    //       headers: {
-    //         'Access-Control-Allow-Origin': '*'
-    //       }
-    //     };
-    //     const headers = { 'Access-Control-Allow-Origin': '*' }
-    //     const response = await axios.post(`${API_URL}`, data, headers);
-    //     console.log(response.data); // handle successful response
-    //   } catch (error) {
-    //     console.error(error); // handle error
-    //   }
-    // };
-    // postData({ name: 'John', email: 'john@example.com' });
-    // postData({ "score": 1 });
-    // ******** axios api *****************//
-
-
-  // ******** fetch api *****************//
-    // const data = {
-    //   score: rateValue,
-    // };
-    
-    // fetch('http://134.122.98.10/api/invite/FhuDa11DTGiq9cvX/rate', {
-    //   method: 'POST',
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'Access-Control-Allow-Origin': '*'
-    //   },
-    //   body: JSON.stringify(data)
-    // })
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch(error => console.error(error));
-  // ******** fetch api *****************//
-  
-  
-  // ******** axios put api *****************//
-  // const data = {
-  //   "score": rateValue
-  // };
-  
-  // axios.post('http://134.122.98.10/api/invite/nUDY6XeDzkIWZx43/rate', data, {
-  //   headers: {
-  //     "accept": "application/json, text/plain, */*",
-  //     "access-control-allow-origin": "*",
-  //     "content-type": "application/json"
-  //   }
-  // })
-  //   .then((response) => {
-  //     // Handle success
-  //     console.log(response.data);
-  //   })
-  //   .catch((error) => {
-  //     // Handle error
-  //     console.error(error);
-  //   });
-  // ******** axios put api *****************//
-
-
-  // ******** axios post json api *****************//
-  const API_URL = `http://134.122.98.10/api/invite/${agentID}/rate`;
-
-  const postData = async (data) => {
-    try {
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*'
+    // ******** axios post RATE *****************//
+    const API_URL_RATE = `http://134.122.98.10/api/invite/${agentID}/rate`;
+    const postDataRate = async (data) => {
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+        };
+        const response = await axios.post(`${API_URL_RATE}`, JSON.stringify(data), config);
+        if (response.status === 201) {
+          console.log(response.data);
+        } else {
+          console.error('Request failed with status code', response.status);
         }
-      };
-      const response = await axios.post(`${API_URL}`, JSON.stringify(data), config);
-      if (response.status === 200) {
-        console.log(response.data); // handle successful response
-      } else {
-        console.error('Request failed with status code', response.status);
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error); // handle error
-    }
-  };
+    };
+    postDataRate({ score: rateValue });
 
-  // postData({ name: 'John', email: 'john@example.com' });
-  postData({ score: rateValue });
 
-  // ******** axios post json api *****************//
+    // ******** axios post COMMENT *****************//
+    const API_URL_COMMENT = `http://134.122.98.10/api/invite/${agentID}/comment`;
+    const postDataComment = async (data) => {
+      try {
+        const config = {
+          headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+        };
+        const response = await axios.post(`${API_URL_COMMENT}`, JSON.stringify(data), config);
+        if (response.status === 201) {
+          console.log(response.data);
+        } else {
+          console.error('Request failed with status code', response.status);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    postDataComment({ comment: commentValue });
 
-    // console.log("newUpdateAgent", ratingScore);
-
-    // Dummy api
-    // api_post();
   };
 
   if (rnrDone) {
